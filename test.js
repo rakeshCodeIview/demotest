e1a4722e3dd03f84f1c6d811297163341bfb260f
jin <rakesh.8@appinventiv.com>
2021-07-02 11:26:11 +0530

    demo code

---
+++ test.js
@@ -0,0 +1,34 @@
+let string = 'akhey';
+let output = '!'
+let prev_row = 0
+let prev_col = 0
+for (let i = 1; i < string.length; i++) {
+    let asci = string[i].charCodeAt(0) - 97
+    // console.log(asci)
+    let curr_row = Math.floor(asci / 5);
+    let curr_col = asci % 5;
+    let diffCol = curr_col - prev_col
+    let diffRow = curr_row - prev_row;
+    if (diffCol < 0) {
+        for (let j = 0; j < Math.abs(diffCol); j++)
+            output += 'L!';
+
+    }
+    else {
+        for (let j = 0; j < Math.abs(diffCol); j++)
+            output += 'R!';
+    }
+    if (diffRow < 0) {
+        for (let j = 0; j < Math.abs(diffRow); j++)
+            output += 'U!';
+    }
+    else {
+        for (let j = 0; j < Math.abs(diffRow); j++)
+            output += 'D!';
+    }
+    prev_row=curr_row;
+    prev_col=curr_col;
+
+}
+
+console.log(output)
